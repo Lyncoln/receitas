d4f3f8f190e607aa857ab8ec35681409db7ea375
Lyncoln <32916183+Lyncoln@users.noreply.github.com>
2019-05-02 16:09:32 -0300

    Update dash

--- testetabela.py
+++ testetabela.py
@@ -4,23 +4,40 @@ Created on Wed May  1 20:06:34 2019
 
 @author: Lyncoln
 """
-
+import dash_table
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import pandas as pd
+import os
+os.chdir("C://Users//tpc 02//Documents//receitas")
+from pesquisador import pesquisa
 
+
+receitas = pd.read_csv("https://github.com/Lyncoln/receitas/raw/master/receitas.csv")
 app = dash.Dash()
 
-app.layout = html.Div(children = [
-        dcc.Input(id = "input", value = "Enter something", type = "text"),
-        html.Div(id = "output")
-        ])
+df = pd.DataFrame(columns = ["nome","ingredientes","site"])
 
-@app.callback(Output(component_id="output", component_property = "children"),
+app.layout = html.Div([html.H3("Entre os ingredientes:"),
+        html.Div(children = [
+        dcc.Input(id = "input", value = "", type = "text"),
+        html.Div(id = "output")
+        ]),
+        dash_table.DataTable(id='table',
+                             columns=[{"name": i, "id": i} for i in df.columns],
+                             data=df.to_dict('records'))
+])
+
+@app.callback(Output(component_id="table", component_property = "data"),
         [Input(component_id = "input", component_property="value")])
 def update_value(input_data):
-    return "Input: {}".format(input_data)
+    
+    testo = input_data.split()
+    tabela = pesquisa(testo)
+    
+    return tabela.to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
